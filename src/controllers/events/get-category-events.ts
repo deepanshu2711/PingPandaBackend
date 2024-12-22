d71a898b5a567a23dcdf240bde4aa195acc4b655
Deepanshu <deepanshusaini2711@gmail.com>
2024-12-22 12:43:55 +0530

    refactor gete category events controller

--- src/controllers/events/get-category-events.ts
+++ src/controllers/events/get-category-events.ts
@@ -1,10 +1,33 @@
 import { Request, Response } from "express";
 import { Event } from "../../model/event";
+import { startOfToday, startOfWeek, startOfMonth } from "date-fns";
 
 export const getCategoryEvents = async (req: Request, res: Response) => {
   const { categoryId } = req.params;
+  const { period } = req.query;
+
+  let filterDate: Date | undefined;
+
+  switch (period) {
+    case "Today":
+      filterDate = startOfToday();
+      break;
+    case "Week":
+      filterDate = startOfWeek(new Date());
+      break;
+    case "Month":
+      filterDate = startOfMonth(new Date());
+      break;
+    default:
+      filterDate = startOfToday();
+      return;
+  }
+
   try {
-    const events = await Event.find({ category: categoryId });
+    const events = await Event.find({
+      eventCategory: categoryId,
+      createdAt: { $gte: filterDate },
+    });
     res.status(200).json({ events: events });
   } catch (error) {
     console.log("GET CATEGORY EVENTS ERROR", error);
